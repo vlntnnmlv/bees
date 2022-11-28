b1812c54e59f5c0935275fa8b34f1d7510f1e9f6
valentinnamleev <v.namleev@zmeke.com>
2022-11-29 02:40:59 +0400

    fixed bee spawning in grid bug

--- Assets/Scripts/Hive.cs
+++ Assets/Scripts/Hive.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Hive : VN.Object
 {
-    Vector2 RandomDiff => new Vector2(Random.Range(-2,2), Random.Range(-2, 2));
     int i = 0;
 
     public static void Create(string _ID, VN.Object _Parent, Vector2 _Offset)
@@ -19,7 +18,7 @@ public class Hive : VN.Object
             if (Vector2.Distance(bee.Offset, Offset) < 1 && bee.GotAPot)
             {
                 bee.DropPot(Offset);
-                Bee.Create($"bee_{i}", null, Offset, Offset + RandomDiff);
+                Bee.Create($"bee_{i}", null, Offset, VN.Utility.RandomOffset);
                 i += 1;
                 HoneyPot.Create("honey_pot", null, VN.Utility.RandomOffset);
             } 
